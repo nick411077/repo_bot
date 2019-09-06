6ae84f5560486f3cf5ab0bbc67cf6f2db3fdb597
nick411077 <nick411077@gmail.com>
2019-09-06 21:06:04 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,10 @@ from discord.ext import commands
 
 TOKEN = 'TOKEN'
 
-bot = commands.Bot(command_prefix = '$')
+bot = commands.Bot(command_prefix='$')
 
 bot.remove_command('help')
+
 
 @bot.command()
 async def load(ctx, extension):
@@ -13,11 +14,13 @@ async def load(ctx, extension):
     await ctx.send(f'load {extension} done')
     print(f'load {extension} done')
 
+
 @bot.command()
 async def unload(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
     await ctx.send(f'unload {extension} done')
     print(f'unload {extension} done')
+
 
 @bot.command()
 async def reload(ctx, extension):
@@ -26,10 +29,9 @@ async def reload(ctx, extension):
     await ctx.send(f'reload {extension} done')
     print(f'reload {extension} done')
 
+
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
-
-
 
 bot.run(TOKEN)
