14e1408ec49e4b7c27920e6168f6513e2b824840
nick411077 <nick411077@gmail.com>
2019-09-30 19:37:38 +0800

    Add project files.

--- config/twitch_api.py
+++
@@ -1,9 +0,0 @@
-import json
-import os
-
-import requests
-
-r = requests.get(os.environ['TWITCH_URL'], headers={'Client-ID': os.environ['TWITCH_TOKEN']})
-data = r.json()
-with open('data.json', 'w', encoding='utf8') as f:
-    json.dump(data, f, ensure_ascii=False)
