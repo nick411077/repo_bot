a0eb04a9f54792589da19728f1e81219b05950d6
nick411077 <nick411077@gmail.com>
2019-09-30 19:59:00 +0800

    Modified grammar

--- cogs/webhooks.py
+++ cogs/webhooks.py
@@ -9,9 +9,13 @@ class Webhook(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.check_status.start()
+
+    def cog_unload(self):
+        self.check_status.cancel()
 
     @tasks.loop(seconds=5)
-    async def check_status(self, message):
+    async def check_status(self):
         r = requests.get(os.environ['TWITCH_URL'], headers={'Client-ID': os.environ['TWITCH_TOKEN']})
         data = r.json()
         with open('../cog/data.json', 'w', encoding='utf8') as wf:
