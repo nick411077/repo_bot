ba349d4e854a25f485b40e16dd50510c2b50290c
nick411077 <nick411077@gmail.com>
2019-10-15 08:04:58 +0800

    Modified grammar

--- testpakage/test.py
+++ testpakage/test.py
@@ -10,18 +10,24 @@ class JSONObject:
         self.__dict__ = d
 
 
-user = {'login': ['summit1g', 'eleaguetv', 'gueigotv', 'chocotaco', 'latteda_', 'zrush']}
+user = {'user_login': ['twnickcan']}
 
-r = requests.get('https://api.twitch.tv/helix/users',
+r = requests.get('https://api.twitch.tv/helix/streams',
                  headers={'Client-ID': 'g3v9rj6v0t5cuthn57g3s9sd1sngmz'},
                  params=user)
 print(r.url)
-g = r.json()
+g = r.json()["data"]
 with open('data.json', 'w', encoding='utf8') as f:
     json.dump(g, f, ensure_ascii=False, indent=4)
 with open('data.json', 'r', encoding='utf8') as rf:
     twitch = json.loads(rf.read(), object_hook=JSONObject)
-user_id = [x.id for x in twitch.data]
+user_id = [x.id for x in twitch]
 
 print(user_id)
 
+if not twitch:
+    stream_info = [dict(live=False, title='', game_id=''), ]
+else:
+    stream_info = [dict(live=True, title=twitch[0].title, game_id=twitch[0].game_id), ]
+
+print(stream_info)
